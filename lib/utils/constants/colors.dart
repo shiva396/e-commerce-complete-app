8deecc36e05029e50ff61887c6f64522ad3067ac
shiva396 <suryashiva396@gmail.com>
2024-02-10 14:29:08 +0530

    Color Constants are added

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -2,7 +2,40 @@ import 'package:flutter/material.dart';
 
 class EColors {
   EColors._();
+
   static const Color primaryColor = Color(0xFF4868FF);
-  
+  static const Color secondaryColor = Color(0xFFFFE24B);
+  static const Color accentColor = Color(0xFFb0c7ff);
+
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF6C757D);
+  static const Color textWhite = Colors.white;
+
+  static const Color light = Colors.white;
+  static const Color dark = Colors.white;
+  static const Color primaryBackground = Colors.white;
+
+  static const Color lightContainer = Color(0xFFF3F5FF);
+  static Color darkContainer = EColors.white.withOpacity(0.1);
+
+  static const Color buttonPrimary = Color(0xFF4b68ff);
+  static const Color buttonSecondary = Color(0xFF6C757D);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
+
+  static const Color borderPrimary = Color(0xFFD9D9D9);
+  static const Color borderSecondary = Color(0xFFD9D9D9);
+
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+  static const Color warning = Color(0xFFF57C00);
+  static const Color info = Color(0xFF1976D2);
+
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
   
 }
